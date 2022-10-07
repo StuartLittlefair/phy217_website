6deab511090e7a1e984315f1306ee1c902babd3d
Stuart Littlefair <s.littlefair@shef.ac.uk>
2022-10-07 09:08:24 +0100

    remove phy241 traces

--- src/_data/site.js
+++ src/_data/site.js
@@ -1,4 +1,4 @@
 module.exports = {
     name: "PHY217",
-    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://slittlefair.staff.shef.ac.uk/teaching/phy241'
+    url: process.env.ELEVENTY_ENV === 'development' ? 'http://localhost:8080' : 'https://slittlefair.staff.shef.ac.uk/teaching/phy217'
 };
